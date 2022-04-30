45793d47215c05a4a5653320d34cdaacc3d51956
Laurène KABA <kabalauren@cy-tech.fr>
2022-05-01 01:24:09 +0200

    Implementation defiler Missile

--- PlaneFlight/Missile.cpp
+++ PlaneFlight/Missile.cpp
@@ -4,19 +4,29 @@ Missile::Missile()
 {
 }
 
+Missile::Missile(int vitesse)
+{
+	myPVitesse = vitesse;
+	this->myPos = { 0.0f , 0.0f };
+	this->myImage = LoadImage("Pictures/Bomb_droite.png");
+
+}
+
 /*void Missile::exploser()
 {
 }*/
 
 void Missile::update()
 {
-	myPos.x += getMyVitesse();
+	this->myPos.x += getMyVitesse();
+	
 }
 
 void Missile::virtualize()
 {
 	// Initialiser texture/image
 	myTexture = LoadTextureFromImage(myImage);
+
 }
 
 Missile::~Missile()
--- PlaneFlight/Missile.h
+++ PlaneFlight/Missile.h
@@ -7,6 +7,7 @@ class Missile : public Projectile
 public:
 	// Constructeur
 	Missile();
+	Missile(int vitesse);
 
 	//Fonctions héritées
 	//void exploser() override;
